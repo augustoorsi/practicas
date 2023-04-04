3c24c5f3d7dd2f49e94d8946b4e725ceba5ab6b2
augustoorsi <augusford@hotmail.com>
2023-04-04 20:45:28 -0300

    4/4 20:45

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Cards from './components/Cards.jsx';
 import Nav from "./components/Nav"
 import axios from "axios"
+import { Route, Routes } from 'react-router-dom';
+import About from "./components/About"
+import Detail from "./components/Detail"
+import Error from './components/Error';
 
 function App() {
    let [characters, setCharacters] = useState([])
@@ -24,8 +28,13 @@ function App() {
 
    return (
       <div className='App'>
-         <Nav onSearch={onSearch}/>
-         <Cards onClose={onClose} characters={characters} />
+         <Nav path=':' onSearch={onSearch}/>
+         <Routes>
+            <Route path="/home" element={<Cards path="/home" onClose={onClose} characters={characters} />}></Route>
+            <Route path='/about' element={<About/>}></Route>
+            <Route path="/detail/:id" element={<Detail/>} ></Route>
+            <Route path='*' element={<Error/>}/>
+         </Routes>
       </div>
    );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,6 @@
+export default function About(){
+    return <div>
+        <h1>Hola, soy Augusto</h1>
+        <p>adsadadadsaadsadsadsdaadsasdadsdasdasdasd</p>
+    </div>
+}
