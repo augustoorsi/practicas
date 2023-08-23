3269e6405af4b6d1a612b83fa0df86bc804efd6f
Augusto Orsi <augusford@hotmail.com>
2023-08-23 14:42:33 -0300

    Delete Desktop/HENRY/PROYECTOS/PI-VIDEOGAMES directory

--- Desktop/HENRY/PROYECTOS/PI-VIDEOGAMES/api/src/routes/videogamesRouter/controller/postVideogameController.js
+++
@@ -1,8 +0,0 @@
-const { Videogame } = require("../../../db")
-
-
-const createVideogame = async (name, description, platforms, image, release_date, rating) =>
-    await Videogame.create({ name, description, platforms, image, release_date, rating })
-
-
-module.exports = { createVideogame }
