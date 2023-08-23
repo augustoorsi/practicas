3b1a7afcbe66851fb6f6325c5c439c21f2208561
Augusto Orsi <augusford@hotmail.com>
2023-08-23 14:42:05 -0300

    Delete Client directory

--- Client/src/components/About/About.jsx
+++
@@ -1,6 +0,0 @@
-export default function About(){
-    return <div>
-        <h1>Hola, soy Augusto</h1>
-        <p>adsadadadsaadsadsadsdaadsasdadsdasdasdasd</p>
-    </div>
-}
--- Client/src/components/Error/Error.jsx
+++
@@ -1,6 +0,0 @@
-const Error = ()=>{
-    return<h1> ERROR!!!!!!!!!!!</h1>
-
-}
-
-export default Error
