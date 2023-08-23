1c683e7c38a03e26cbfc6eb463a9c044e8181d41
augustoorsi <augusford@hotmail.com>
2023-08-23 14:27:41 -0300

    tw 00

---
+++ Desktop/Projectos/Pruebas React/projects/00/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Cards from './Views/Cards/Cards'
+import "./App.css"
+
+function App() {
+  return (
+    <>
+    <Cards></Cards>
+    </>
+  )
+}
+
+export default App
---
+++ Desktop/Projectos/Pruebas React/projects/00/src/Views/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import Card from "../../components/Card/Card"
+
+function Cards(){
+    return(
+        <>
+            <Card></Card>
+        </>
+    )
+}
+
+export default Cards
